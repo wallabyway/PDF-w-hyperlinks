77ff262bb511f202b80ed6fc149d42f1479dfb79
wallabyway <bealester@gmail.com>
2023-02-02 14:14:37 -0800

    added context menu jump to 3D

--- docs/hyperlinkpdf.ext.js
+++ docs/hyperlinkpdf.ext.js
@@ -1,8 +1,12 @@
+// PURPOSE: Extension adds hyperlinks to 2D PDF (called bubbles in Inventor).
+// When a bubble is clicked a custom context-menu popup appears, which opens other 2D/3D drawings
+
 const AV = Autodesk.Viewing;
 const HIGHLIGHT_ON = 0.3;
 const HIGHLIGHT_OFF = 0.01;
 const HIGHLIGHT_COLOR = 0x1f00ff;
 const OVERLAY = 'bubbles-scene';
+const BUBBLE_RADIUS = 0.18;
 
 ///////////////////////////////////////////////////////////////////
 // load hyperlink extension 
@@ -14,12 +18,15 @@ const OVERLAY = 'bubbles-scene';
 
 	async load() { 
 		await this.viewer.waitForLoadDone();
+
+		// load bubble module
 		if (!this.viewer.overlays.hasScene(OVERLAY)) {
 			this.viewer.overlays.addScene(OVERLAY);
 		}
 		this.tool = new hyperlinkTool(this.viewer);
 		this.viewer.toolController.registerTool(this.tool);
-		this.viewer.toolController.activateTool(OVERLAY)
+		this.viewer.toolController.activateTool(OVERLAY);
+
 		console.log('hyperlinksPdf...loaded');
 		return true;
 	}
@@ -28,7 +35,41 @@ const OVERLAY = 'bubbles-scene';
 AV.theExtensionManager.registerExtension("hyperlinksPdf", hyperlinksPdf);
 
 ///////////////////////////////////////////////////////////////////
-// tool control for mouse events
+// Custom Context Menu
+class CMenuExtension extends Autodesk.Viewing.UI.ObjectContextMenu {
+	constructor(opts) {
+		super(opts);
+	};
+
+	buildMenu(event, node) {
+		var menu = [];
+		this.name = "lalala";
+		this.dbid = 123;
+		menu.push({
+			title: `open part ${event.partID}`,
+			className: 'navbar',
+			target: (e) => this.launchView(event)
+		}, {
+			title: `Hide 123 ${event.url}`,
+			className: 'navbar disabled',
+			target: (e) => viewer.hide(event.partID)
+		});
+		return menu;
+	}
+
+	launchView(event) {
+		AV.Document.load(`${event.url}`, async (doc) => {
+			var viewables = doc.getRoot().getDefaultGeometry();
+			this.viewer.loadDocumentNode(doc, viewables);
+			await this.viewer.waitForLoadDone();
+			this.viewer.isolate(event.partID);
+			this.viewer.fitToView(event.partID);
+		});
+	}
+}
+
+///////////////////////////////////////////////////////////////////
+// Add Hyperlinks to Canvas.  Bubbles are made of 3js circle geometry. Hit test via a ToolInterface mouse events
 class hyperlinkTool extends Autodesk.Viewing.ToolInterface {
     constructor(viewer) {
         super();
@@ -39,12 +80,16 @@ class hyperlinkTool extends Autodesk.Viewing.ToolInterface {
 		this.names = [OVERLAY];
 		this.viewer = viewer;
 		this.layer = this.viewer.overlays.impl.overlayScenes[OVERLAY];
+
+		// load custom context menu module
+		this.menu = new CMenuExtension(this.viewer);//new Autodesk.Viewing.UI.ObjectContextMenu(this.viewer);
+		this.viewer.setContextMenu(this.menu);		
 	}
 
 	register() {
-		this.addBubble("open part3", "some3.pdf", 9.312, 14.85);
-		this.addBubble("open level2", "another.pdf",  9.4414, 12.926);
-		this.addBubble("open level1", "another1.pdf",  9.47, 11.4);
+		this.addBubble(10, "urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6c2YtYWNjZWwtMS9NZXRhbCUyMENvbnRhaW5lci5kd2Y", 9.312, 14.85);
+		this.addBubble("level2123", "another.pdf",  9.4414, 12.926);
+		this.addBubble("level1456", "another1.pdf",  9.47, 11.4);
     }
 
     getPriority() {
@@ -53,7 +98,12 @@ class hyperlinkTool extends Autodesk.Viewing.ToolInterface {
 
 	handleSingleClick(event) {
 		const intersects = this.intersect(event, this.layer.scene.children);
-		if (intersects[0]) alert(intersects[0].tooltip)
+		if (intersects[0]) {
+			console.log(intersects[0].partID);
+			event.partID = intersects[0].partID;
+			event.url = intersects[0].url;
+			this.menu.show(event);
+		}
         return false;
     };
 
@@ -80,13 +130,13 @@ class hyperlinkTool extends Autodesk.Viewing.ToolInterface {
 		this.viewer.impl.invalidate(true);
 	}
 
-	addBubble(tooltip, url, x, y) {
-		const geom = new THREE.CircleGeometry( 0.18, 32 );
+	addBubble(partID, url, x, y) {
+		const geom = new THREE.CircleGeometry( BUBBLE_RADIUS, 32 );
 		const material = new THREE.MeshBasicMaterial({ color: HIGHLIGHT_COLOR });
 		material.opacity=HIGHLIGHT_OFF;
 		const sphereMesh = new THREE.Mesh(geom, material);
 		sphereMesh.position.set(x, y, 0);
-		sphereMesh.tooltip = tooltip;
+		sphereMesh.partID = partID;
 		sphereMesh.url = url;
 		this.viewer.overlays.addMesh(sphereMesh, OVERLAY);
 	}
