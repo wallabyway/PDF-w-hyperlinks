3e87c103b9c9c7bdb5dd9e248a3fe937d4803848
Christian Gessner <christian@gessner.cc>
2023-02-02 23:27:59 -0800

    Changed file name

--- docs/app.js
+++ docs/app.js
@@ -8,7 +8,7 @@ async function startViewer() {
 	AV.Initializer({ env: 'AutodeskProduction', accessToken: _access_token, }, async function () {
 		const viewer = new Autodesk.Viewing.GuiViewer3D(
 			document.getElementById('Viewer'),
-			{ extensions: ["hyperlinksPdf", "docBrowser", "Autodesk.PDF"] }
+			{ extensions: ["hyperlinksPdf", "docBrowser", "Autodesk.PDF", "Autodesk.DWF"] }
 		);
 		viewer.start();
 		viewer.setTheme('light-theme');
